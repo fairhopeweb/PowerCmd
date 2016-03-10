f0b98d02d9593e1326b09172bc4f838d3dbf417e
Rico Suter <mail@rsuter.com>
2016-03-10 23:27:10 +0100

    Improved PoC

--- src/PowerCmd/MainWindow.xaml.cs
+++ src/PowerCmd/MainWindow.xaml.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using MyToolkit.Storage;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -22,16 +27,35 @@ namespace PowerCmd
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int _maxTextLength = 1024 * 128;
         private Process _process;
+
+        public ObservableCollection<string> History { get; } = new ObservableCollection<string>();
 
         public MainWindow()
         {
             InitializeComponent();
+
             Loaded += OnLoaded;
+            Closed += OnClosed;
+
+            DataContext = this;
+            //Input.Loaded += myCombo_Loaded;
         }
+
+        //private void myCombo_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        //{
+        //    ControlTemplate ct = Input.Template;
+        //    Popup pop = ct.FindName("PART_Popup", Input) as Popup;
+        //    pop.Placement = PlacementMode.Top;
+        //}
 
         private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
         {
+            var currentDirectory = ApplicationSettings.GetSetting("CurrentDirectory", "C:/");
+            if (Directory.Exists(currentDirectory))
+                Directory.SetCurrentDirectory(currentDirectory);
+
             Input.Focus();
 
             _process = Process.Start(new ProcessStartInfo("cmd.exe")
@@ -48,7 +72,7 @@ namespace PowerCmd
 
             var outputThread = new Thread(new ParameterizedThreadStart(delegate
             {
-                var buffer = new char[1024 * 10];
+                var buffer = new char[1024 * 128];
                 while (true)
                 {
                     var count = _process.StandardOutput.Read(buffer, 0, buffer.Length);
@@ -66,7 +90,7 @@ namespace PowerCmd
 
             var errorThread = new Thread(new ParameterizedThreadStart(delegate
             {
-                var buffer = new char[1024];
+                var buffer = new char[1024 * 128];
                 while (true)
                 {
                     var count = _process.StandardError.Read(buffer, 0, buffer.Length);
@@ -78,12 +102,22 @@ namespace PowerCmd
 
             _process.Exited += (s, eventArgs) =>
             {
-                Close();
+                Dispatcher.InvokeAsync(() =>
+                {
+                    Close();
+                });
             };
         }
 
+        private void OnClosed(object sender, EventArgs eventArgs)
+        {
+            var match = Regex.Match(Output.Text, "^.*?(\n(.*))>$", RegexOptions.Multiline);
+            if (match.Success)
+                ApplicationSettings.SetSetting("CurrentDirectory", match.Groups[2].Value, false, true);
+        }
+
         private StringBuilder _output = new StringBuilder();
-        private bool _updating = false; 
+        private bool _updating = false;
 
         private void AddText(char[] buffer, int count)
         {
@@ -96,14 +130,14 @@ namespace PowerCmd
 
                     Dispatcher.InvokeAsync(() =>
                     {
-                        var text = ""; 
+                        var text = "";
                         lock (_output)
                         {
-                            text = _output.Length > 100 ? _output.ToString(_output.Length - 100, 100) : _output.ToString();
+                            text = _output.Length > _maxTextLength ? _output.ToString(_output.Length - _maxTextLength, _maxTextLength) : _output.ToString();
                             _updating = false;
                         }
 
-                        Output.Text = text; 
+                        Output.Text = text;
                         Output.ScrollToEnd();
                     });
                 }
@@ -114,14 +148,20 @@ namespace PowerCmd
         {
             if (e.Key == Key.Enter)
             {
-                _process.StandardInput.WriteLine(Input.Text);
+                WriteCommand(Input.Text);
                 Input.Text = "";
             }
         }
 
         public void RunCommand(Command command)
         {
-            _process.StandardInput.WriteLine(command.Text);
+            WriteCommand(command.Text);
+        }
+
+        private void WriteCommand(string command)
+        {
+            History.Insert(0, command);
+            _process.StandardInput.WriteLine(command);
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
@@ -130,6 +170,24 @@ namespace PowerCmd
             {
                 Text = @"""C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"""
             });
+            Input.Focus();
+        }
+        private void ButtonBase_OnClick2(object sender, RoutedEventArgs e)
+        {
+            RunCommand(new Command
+            {
+                Text = @"""C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"""
+            });
+            Input.Focus();
+        }
+
+        private void ButtonBase_OnClick3(object sender, RoutedEventArgs e)
+        {
+            RunCommand(new Command
+            {
+                Text = @"""C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\VsDevCmd.bat"""
+            });
+            Input.Focus();
         }
     }
 
